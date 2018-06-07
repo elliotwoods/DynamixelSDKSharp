4992105a9cc65c6c8151bbfbd63fa2d48c28e507
Arron Smith <arronsmith@gmail.com>
2018-06-07 13:44:38 +0100

    Dispatcher: Heliostat 'nudge' command, but really it should be called 'home'.

---
+++ Samples/Dispatcher/Dispatcher/Requests/Heliostat/Nudge.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using DynamixelSDKSharp;
+
+namespace Dispatcher.Requests.Heliostat
+{
+    [RequestHandler(Method = Method.POST)]
+    [Serializable]
+    class Nudge : IRequest 
+    {
+        public int heliostatID { get; set; }
+
+        public Nudge()
+        {
+
+        }
+
+        public object Perform()
+        {
+            var hstat = Program.Heliostats.Where(h => h.ID == heliostatID).SingleOrDefault();
+            if (hstat == null)
+            {
+                throw new Exception(String.Format("Heliostat Nudge: Couldn't find heliostat with ID {0}", heliostatID));
+            }
+
+            if (hstat.axis1Servo == null)
+            {
+                throw new Exception(String.Format("Heliostat Nudge: Couldn't find servo for axis 1"));
+            }
+
+            if (hstat.axis2Servo == null)
+            {
+                throw new Exception(String.Format("Heliostat Nudge: Couldn't find servo for axis 2"));
+            }
+
+            hstat.axis1Servo.WriteValue(RegisterType.GoalPosition, 2048);
+            hstat.axis2Servo.WriteValue(RegisterType.GoalPosition, 2048);
+
+            return new { };
+        }
+    }
+}
